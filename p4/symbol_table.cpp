b393f82433e973b072b6b3187ce3d86298bc0e03
Michael Sawyer <michaelsawyer92@gmail.com>
2014-02-15 12:51:21 -0800

    Added Symbol_table, print not implemented yet.

---
+++ p4/symbol_table.cpp
@@ -0,0 +1,11 @@
+#include "symbol_table.h"
+
+/* static */ Symbol_table *Symbol_table::m_instance = 0;
+
+/* static */ Symbol_table * Symbol_table::instance()
+{
+  if (m_instance == 0)
+    m_instance = new Symbol_table();
+  return m_instance;
+}
+
