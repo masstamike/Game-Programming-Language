de127b58d0efb3abc03deac4cde376bfd298c148
Michael Sawyer <michaelsawyer92@gmail.com>
2014-03-09 18:42:27 -0700

    Added variable class, making progress. Focus on passing t700.

---
+++ p5/variable.cpp
@@ -0,0 +1,7 @@
+#include variable.h
+
+Variable::Variable (std::string id, Symbol* sym, std::string type) {
+    m_id = id;
+    m_sym = sym;
+    m_type = type;
+}
---
+++ p5/variable.h
@@ -0,0 +1,14 @@
+#include <string>
+
+#ifndef VARIABLE_H
+#define VARIABLE_H
+
+class Variable {
+    public:
+        Variable(std::string, Symbol*, std::string);
+
+    private:
+        
+};
+
+#endif
